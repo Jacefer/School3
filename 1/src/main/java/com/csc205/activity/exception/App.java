bf9072a6bdadb3f195f6e2c7708ec7e569757223
Jacefer <71197991+Jacefer@users.noreply.github.com>
2024-02-13 20:26:33 -0700

    Add files via upload

---
+++ 1/src/main/java/com/csc205/activity/exception/App.java
@@ -0,0 +1,13 @@
+package com.csc205.activity.exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ 1/src/main/java/com/csc205/activity/exception/ExceptionActivity.java
@@ -0,0 +1,61 @@
+package com.csc205.activity.exception;
+
+import java.util.Scanner;
+
+public class ExceptionActivity {
+	public static void main(String[] args) {
+	// try-catch
+	try {
+	    // happy path - all this executes if no exception thrown
+	} catch (Exception e) {
+	    // sad path - exception thrown
+	    e.printStackTrace();
+	}
+	// do more stuff
+	
+	// try-catch-catch - order of catch blocks matters per hierarchy
+	try {
+	    // Do something here
+	} catch (NullPointerException e) {
+	    // we'll catch the NullPointerException here and handle it differently than other exceptions
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	// do more stuff
+	
+	try {
+	    // do something
+	} catch (Exception e) {
+	    e.printStackTrace();
+	} finally {
+	    // always do something
+	}
+	// do yet more stuff
+	
+	try {
+		  // do something we don't expect to throw exceptions
+		} finally {
+		  // but if we do, always execute code here
+		}
+		// more stuff here
+	
+	// java 7 multi-try-catch
+	try {
+	    
+	} catch (ArithmeticException | NullPointerException e) {
+	    // when either of the exceptions are thrown, do this
+	    e.printStackTrace();
+	}
+	// yep, you guessed it - more stuff
+	
+	// try-with-resources
+	try(Scanner scanner1 = new Scanner(System.in)){
+	   int MyNumber = scanner1.nextInt();
+	   System.out.println("< "+MyNumber+" >");
+	}
+	catch (Exception e) {
+		System.out.println("Error:");
+	    e.printStackTrace();
+		}
+	}
+}
